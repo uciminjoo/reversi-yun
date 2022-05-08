25528de5a88509cc8395d0a5aa1fb0fbaf499444
Minjoo Yun <heyheyminjoo@gmail.com>
2022-05-08 13:33:57 -0700

    server.js error fixed

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 /***************************/
 /* Set up the static file server */
-let static = require('node.static');
+let static = require('node-static');
 
 /* Set up the http server library */
 let http = require('http');
 
 /* Assume that we are running on Heroku */
 let port = process.env.PORT;
-let directory = __dirname + '/public'
+let directory = __dirname + '/public';
 
 /* If we aren't on Heroku, then we need to adjust our port and directory */
 if ((typeof port == 'undefined') || (port === null)){
@@ -16,7 +16,8 @@ if ((typeof port == 'undefined') || (port === null)){
 }
 
 /* Set up our static file web server to deliver files from the filesystem */
-let file = new static.server(directory);
+let file = new static.Server(directory);
+
 let app = http.createServer(
 	function(request, response){
 		request.addListener('end',
