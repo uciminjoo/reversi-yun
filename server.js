8ce8c90745fbfd5c5bba45ea3b04bddfdbaf2f0f
Minjoo Yun <heyheyminjoo@gmail.com>
2022-05-08 11:13:25 -0700

    server.js

---
+++ server.js
@@ -0,0 +1,31 @@
+/***************************/
+/* Set up the static file server */
+let static = require('node.static');
+
+/* Set up the http server library */
+let http = require('http');
+
+/* Assume that we are running on Heroku */
+let port = process.env.PORT;
+let directory = __dirname + '/public'
+
+/* If we aren't on Heroku, then we need to adjust our port and directory */
+if ((typeof port == 'undefined') || (port === null)){
+	port = 8080;
+	directory = './public';
+}
+
+/* Set up our static file web server to deliver files from the filesystem */
+let file = new static.server(directory);
+let app = http.createServer(
+	function(request, response){
+		request.addListener('end',
+			function(){
+				file.serve(request, response);
+			}
+		)
+	}
+
+).listen(port);
+
+console.log('The server is running')
